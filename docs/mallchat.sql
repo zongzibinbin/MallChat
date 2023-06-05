90ffdc26fbbcf8fe08f9cf482943e6ac3c5185af
yinpeng <y960303802@163.com>
2023-06-05 23:28:43 +0800

    fix: mallchat.sql 相关修改

--- docs/mallchat.sql
+++ docs/mallchat.sql
@@ -7,24 +7,23 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `item_config`;
 CREATE TABLE `item_config`  (
   `id` bigint(20) UNSIGNED NOT NULL COMMENT 'id',
-  `type` int(11) NOT NULL COMMENT '物品类型 1改名卡 2徽章',
+  `type` int(11) UNSIGNED NOT NULL COMMENT '物品类型 1改名卡 2徽章',
   `img` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '物品图片',
   `describe` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '物品功能描述',
   `create_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT '创建时间',
   `update_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3) COMMENT '修改时间',
   PRIMARY KEY (`id`) USING BTREE,
-  INDEX `idx_create_time`(`create_time`) USING BTREE,
-  INDEX `idx_update_time`(`update_time`) USING BTREE
+  INDEX `idx_create_time`(`create_time`) USING BTREE COMMENT '创建时间索引',
+  INDEX `idx_update_time`(`update_time`) USING BTREE COMMENT '修改时间索引'
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '功能物品配置表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of item_config
 -- ----------------------------
 INSERT INTO `item_config` VALUES (1, 1, NULL, '用户可以使用改名卡，更改自己的名字。mallchat名称全局唯一，快抢订你的专属昵称吧', '2023-03-25 22:27:30.511', '2023-03-25 22:27:30.511');
-INSERT INTO `item_config` VALUES (2, 2, 'https://cdn-icons-png.flaticon.com/128/1533/1533913.png', '爆赞徽章，单条消息被点赞超过10次，即可获得', '2023-05-07 17:50:31.090', '2023-05-07 18:12:05.824');
+INSERT INTO `item_config` VALUES (2, 2, 'https://cdn-icons-png.flaticon.com/512/10667/10667168.png ', '爆赞徽章，单条消息被点赞超过10次，即可获得', '2023-05-07 17:50:31.090', '2023-05-07 18:12:05.824');
 INSERT INTO `item_config` VALUES (3, 2, 'https://cdn-icons-png.flaticon.com/512/6198/6198527.png ', '抹茶聊天前10名注册的用户才能获得的专属徽章', '2023-05-07 17:50:31.100', '2023-05-07 18:12:01.448');
 INSERT INTO `item_config` VALUES (4, 2, 'https://cdn-icons-png.flaticon.com/512/10232/10232583.png', '抹茶聊天前100名注册的用户才能获得的专属徽章', '2023-05-07 17:50:31.109', '2023-05-07 17:56:36.059');
-INSERT INTO `item_config` VALUES (5, 2, 'https://cdn-icons-png.flaticon.com/128/2909/2909937.png', '抹茶知识星球成员的专属徽章', '2023-05-07 17:50:31.109', '2023-05-07 17:56:36.059');
 
 -- ----------------------------
 -- Table structure for message
@@ -32,22 +31,22 @@ INSERT INTO `item_config` VALUES (5, 2, 'https://cdn-icons-png.flaticon.com/128/
 DROP TABLE IF EXISTS `message`;
 CREATE TABLE `message`  (
   `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'id',
-  `room_id` bigint(20) NOT NULL COMMENT '会话表id',
-  `from_uid` bigint(20) NOT NULL COMMENT '消息发送者uid',
-  `content` varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '消息内容',
+  `room_id` bigint(20) UNSIGNED NOT NULL COMMENT '会话表id',
+  `from_uid` bigint(20) UNSIGNED NOT NULL COMMENT '消息发送者uid',
+  `content` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '消息内容',
   `reply_msg_id` bigint(20) NULL DEFAULT NULL COMMENT '回复的消息内容',
-  `status` int(11) NOT NULL COMMENT '消息状态 0正常 1删除',
-  `gap_count` int(11) NULL DEFAULT NULL COMMENT '与回复的消息间隔多少条',
-  `type` int(11) NULL DEFAULT 1 COMMENT '消息类型 1正常文本 2.撤回消息',
+  `status` int(11) UNSIGNED NOT NULL COMMENT '消息状态 0正常 1删除',
+  `gap_count` int(11) UNSIGNED NULL DEFAULT NULL COMMENT '与回复的消息间隔多少条',
+  `type` int(11) UNSIGNED NULL DEFAULT 1 COMMENT '消息类型 1正常文本 2.爆赞 （点赞超过10）3.危险发言（举报超5）',
   `extra` json DEFAULT NULL COMMENT '扩展信息',
   `create_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT '创建时间',
   `update_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3) COMMENT '修改时间',
   PRIMARY KEY (`id`) USING BTREE,
-  INDEX `idx_room_id`(`room_id`) USING BTREE,
-  INDEX `idx_from_uid`(`from_uid`) USING BTREE,
-  INDEX `idx_create_time`(`create_time`) USING BTREE,
-  INDEX `idx_update_time`(`update_time`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 5709 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '消息表' ROW_FORMAT = Dynamic;
+  INDEX `idx_room_id`(`room_id`) USING BTREE COMMENT '会话表id索引',
+  INDEX `idx_from_uid`(`from_uid`) USING BTREE COMMENT '消息发送者uid索引',
+  INDEX `idx_create_time`(`create_time`) USING BTREE COMMENT '创建时间索引',
+  INDEX `idx_update_time`(`update_time`) USING BTREE COMMENT '修改时间索引'
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '消息表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for message_mark
@@ -55,18 +54,18 @@ CREATE TABLE `message`  (
 DROP TABLE IF EXISTS `message_mark`;
 CREATE TABLE `message_mark`  (
   `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'id',
-  `msg_id` bigint(20) NOT NULL COMMENT '消息表id',
-  `uid` bigint(20) NOT NULL COMMENT '标记人uid',
-  `type` int(11) NOT NULL COMMENT '标记类型 1点赞 2举报',
-  `status` int(11) NOT NULL COMMENT '消息状态 0正常 1取消',
+  `msg_id` bigint(20) UNSIGNED NOT NULL COMMENT '消息表id',
+  `uid` bigint(20) UNSIGNED NOT NULL COMMENT '标记人uid',
+  `type` int(11) UNSIGNED NOT NULL COMMENT '标记类型 1点赞 2举报',
+  `status` int(11) UNSIGNED NOT NULL COMMENT '消息状态 0正常 1取消',
   `create_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT '创建时间',
   `update_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3) COMMENT '修改时间',
   PRIMARY KEY (`id`) USING BTREE,
-  INDEX `idx_msg_id`(`msg_id`) USING BTREE,
-  INDEX `idx_uid`(`uid`) USING BTREE,
-  INDEX `idx_create_time`(`create_time`) USING BTREE,
-  INDEX `idx_update_time`(`update_time`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '消息标记表' ROW_FORMAT = Dynamic;
+  INDEX `idx_msg_id`(`msg_id`) USING BTREE COMMENT '消息表id索引',
+  INDEX `idx_uid`(`uid`) USING BTREE COMMENT 'uid索引',
+  INDEX `idx_create_time`(`create_time`) USING BTREE COMMENT '创建时间索引',
+  INDEX `idx_update_time`(`update_time`) USING BTREE COMMENT '修改时间索引'
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '消息标记表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for room
@@ -75,15 +74,15 @@ DROP TABLE IF EXISTS `room`;
 CREATE TABLE `room`  (
   `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'id',
   `name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '会话名',
-  `type` int(11) NOT NULL COMMENT '会话类型 1大群聊 2沸点',
+  `type` int(11) UNSIGNED NOT NULL COMMENT '会话类型 1大群聊 2沸点',
   `active_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT '最后活跃时间-排序',
   `create_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT '创建时间',
   `update_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3) COMMENT '修改时间',
   PRIMARY KEY (`id`) USING BTREE,
-  INDEX `idx_active_time`(`active_time`) USING BTREE,
-  INDEX `idx_create_time`(`create_time`) USING BTREE,
-  INDEX `idx_update_time`(`update_time`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '会话表' ROW_FORMAT = Dynamic;
+  INDEX `idx_active_time`(`active_time`) USING BTREE COMMENT '活跃时间索引',
+  INDEX `idx_create_time`(`create_time`) USING BTREE COMMENT '创建时间索引',
+  INDEX `idx_update_time`(`update_time`) USING BTREE COMMENT '修改时间索引'
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '会话表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of room
@@ -98,20 +97,20 @@ CREATE TABLE `user`  (
   `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '用户id',
   `name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '用户昵称',
   `avatar` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '用户头像',
-  `sex` int(11) NULL DEFAULT NULL COMMENT '性别 1为男性，2为女性',
+  `sex` int(11) UNSIGNED NULL DEFAULT NULL COMMENT '性别 1为男性，2为女性',
   `open_id` char(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '微信openid用户标识',
   `last_opt_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT '最后上下线时间',
   `ip_info` json NULL COMMENT 'ip信息',
-  `item_id` bigint(20) NULL DEFAULT NULL COMMENT '佩戴的徽章id',
-  `status` int(11) DEFAULT "0" COMMENT '使用状态 0.正常 1拉黑',
+  `item_id` bigint(20) UNSIGNED NULL DEFAULT NULL COMMENT '佩戴的徽章id',
+  `status` int(11) UNSIGNED DEFAULT '0' COMMENT '使用状态 0.正常 1拉黑',
   `create_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT '创建时间',
   `update_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3) COMMENT '修改时间',
   PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE INDEX `uniq_open_id`(`open_id`) USING BTREE,
-  UNIQUE INDEX `uniq_name`(`name`) USING BTREE,
-  INDEX `idx_create_time`(`create_time`) USING BTREE,
-  INDEX `idx_update_time`(`update_time`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 10024 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
+  UNIQUE INDEX `uniq_open_id`(`open_id`) USING BTREE COMMENT '微信openid唯一索引',
+  UNIQUE INDEX `uniq_name`(`name`) USING BTREE COMMENT '用户昵称唯一索引',
+  INDEX `idx_create_time`(`create_time`) USING BTREE COMMENT '创建时间索引',
+  INDEX `idx_update_time`(`update_time`) USING BTREE COMMENT '修改时间索引'
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for user_backpack
@@ -119,18 +118,18 @@ CREATE TABLE `user`  (
 DROP TABLE IF EXISTS `user_backpack`;
 CREATE TABLE `user_backpack`  (
   `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'id',
-  `uid` bigint(20) NOT NULL COMMENT 'uid',
-  `item_id` int(11) NOT NULL COMMENT '物品id',
-  `status` int(11) NOT NULL COMMENT '使用状态 0.待使用 1已使用',
+  `uid` bigint(20) UNSIGNED NOT NULL COMMENT 'uid',
+  `item_id` int(11) UNSIGNED NOT NULL COMMENT '物品id',
+  `status` int(11) UNSIGNED NOT NULL COMMENT '使用状态 0.待使用 1已使用',
   `idempotent` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '幂等号',
   `create_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT '创建时间',
   `update_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3) COMMENT '修改时间',
   PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE INDEX `uniq_idempotent`(`idempotent`) USING BTREE,
-  INDEX `idx_uid`(`uid`) USING BTREE,
-  INDEX `idx_create_time`(`create_time`) USING BTREE,
-  INDEX `idx_update_time`(`update_time`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '用户背包表' ROW_FORMAT = Dynamic;
+  UNIQUE INDEX `uniq_idempotent`(`idempotent`) USING BTREE COMMENT '幂等唯一索引',
+  INDEX `idx_uid`(`uid`) USING BTREE COMMENT 'uid索引',
+  INDEX `idx_create_time`(`create_time`) USING BTREE COMMENT '创建时间索引',
+  INDEX `idx_update_time`(`update_time`) USING BTREE COMMENT '修改时间索引'
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '用户背包表' ROW_FORMAT = Dynamic;
 
 DROP TABLE IF EXISTS `wx_msg`;
 CREATE TABLE `wx_msg`  (
@@ -140,21 +139,21 @@ CREATE TABLE `wx_msg`  (
   `create_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT '创建时间',
   `update_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3) COMMENT '修改时间',
   PRIMARY KEY (`id`) USING BTREE,
-  INDEX `idx_open_id`(`open_id`) USING BTREE,
-  INDEX `idx_create_time`(`create_time`) USING BTREE,
-  INDEX `idx_update_time`(`update_time`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '微信消息表' ROW_FORMAT = Dynamic;
+  INDEX `idx_open_id`(`open_id`) USING BTREE COMMENT '微信openid索引',
+  INDEX `idx_create_time`(`create_time`) USING BTREE COMMENT '创建时间索引',
+  INDEX `idx_update_time`(`update_time`) USING BTREE COMMENT '修改时间索引'
+) ENGINE=InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '微信消息表' ROW_FORMAT = Dynamic;
 
 DROP TABLE IF EXISTS `black`;
 CREATE TABLE `black`  (
   `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'id',
-  `type` int(11) NOT NULL COMMENT '拉黑目标类型 1.ip 2uid',
+  `type` int(11) UNSIGNED NOT NULL COMMENT '拉黑目标类型 1.ip 2uid',
   `target` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '拉黑目标',
   `create_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT '创建时间',
   `update_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3) COMMENT '修改时间',
   PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE INDEX `idx_type_target`(`type`, `target`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '黑名单' ROW_FORMAT = Dynamic;
+  UNIQUE INDEX `idx_type_target`(`type`, `target`) USING BTREE COMMENT '拉黑类型和目标唯一索引'
+) ENGINE=InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '黑名单' ROW_FORMAT = Dynamic;
 
 DROP TABLE IF EXISTS `role`;
 CREATE TABLE `role` (
@@ -163,9 +162,9 @@ CREATE TABLE `role` (
   `create_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT '创建时间',
   `update_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3) COMMENT '修改时间',
   PRIMARY KEY (`id`) USING BTREE,
-  KEY `idx_create_time` (`create_time`) USING BTREE,
-  KEY `idx_update_time` (`update_time`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='角色表';
+  KEY `idx_create_time` (`create_time`) USING BTREE COMMENT '创建时间索引',
+  KEY `idx_update_time` (`update_time`) USING BTREE COMMENT '修改时间索引'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='角色表';
 insert into role(id,`name`) values(1,'超级管理员');
 insert into role(id,`name`) values(2,'抹茶群聊管理员');
 
@@ -177,8 +176,8 @@ CREATE TABLE `user_role` (
   `create_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT '创建时间',
   `update_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3) COMMENT '修改时间',
   PRIMARY KEY (`id`) USING BTREE,
-  KEY `idx_uid` (`uid`) USING BTREE,
-  KEY `idx_role_id` (`role_id`) USING BTREE,
-  KEY `idx_create_time` (`create_time`) USING BTREE,
-  KEY `idx_update_time` (`update_time`) USING BTREE
+  KEY `idx_uid` (`uid`) USING BTREE COMMENT 'uid索引',
+  KEY `idx_role_id` (`role_id`) USING BTREE COMMENT '角色id索引',
+  KEY `idx_create_time` (`create_time`) USING BTREE COMMENT '创建时间索引',
+  KEY `idx_update_time` (`update_time`) USING BTREE COMMENT '修改时间索引'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='用户角色关系表';
