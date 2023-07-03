ec76c2e337309d79e5bd0aafc7c5acc3aebdee9d
zhaoyuhang <1045078399@qq.com>
2023-07-03 11:31:11 +0800

    fix:ChatAI优化

--- mallchat-custom-server/src/main/java/com/abin/mallchat/custom/chatai/handler/ChatAIHandlerFactory.java
+++ mallchat-custom-server/src/main/java/com/abin/mallchat/custom/chatai/handler/ChatAIHandlerFactory.java
@@ -1,7 +1,6 @@
 package com.abin.mallchat.custom.chatai.handler;
 
 import com.baomidou.mybatisplus.core.toolkit.CollectionUtils;
-import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 
 import java.util.List;
 import java.util.Map;
@@ -9,11 +8,9 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class ChatAIHandlerFactory {
     private static final Map<Long, AbstractChatAIHandler> CHATAI_ID_MAP = new ConcurrentHashMap<>();
-    private static final Map<String, AbstractChatAIHandler> CHATAI_NAME_MAP = new ConcurrentHashMap<>();
 
-    public static void register(Long aIUserId, String name, AbstractChatAIHandler chatAIHandler) {
+    public static void register(Long aIUserId, AbstractChatAIHandler chatAIHandler) {
         CHATAI_ID_MAP.put(aIUserId, chatAIHandler);
-        CHATAI_NAME_MAP.put(name, chatAIHandler);
     }
 
     public static AbstractChatAIHandler getChatAIHandlerById(List<Long> userIds) {
@@ -24,17 +21,6 @@ public class ChatAIHandlerFactory {
             AbstractChatAIHandler chatAIHandler = CHATAI_ID_MAP.get(userId);
             if (chatAIHandler != null) {
                 return chatAIHandler;
-            }
-        }
-        return null;
-    }
-    public static  AbstractChatAIHandler getChatAIHandlerByName(String text) {
-        if (StringUtils.isBlank(text)) {
-            return null;
-        }
-        for (Map.Entry<String, AbstractChatAIHandler> entry : CHATAI_NAME_MAP.entrySet()) {
-            if (text.contains("@"+entry.getKey())) {
-                return entry.getValue();
             }
         }
         return null;
