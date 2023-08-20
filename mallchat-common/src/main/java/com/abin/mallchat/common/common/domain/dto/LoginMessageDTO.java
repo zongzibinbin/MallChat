fd7eac8a22aa1abd4078c89098ca80f214663390
zhongzb <972627721@qq.com>
2023-08-20 23:01:12 +0800

    fix: 1.代码优化

--- mallchat-common/src/main/java/com/abin/mallchat/common/common/domain/dto/LoginMessageDTO.java
+++ mallchat-common/src/main/java/com/abin/mallchat/common/common/domain/dto/LoginMessageDTO.java
@@ -2,6 +2,7 @@ package com.abin.mallchat.common.common.domain.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
@@ -12,6 +13,7 @@ import java.io.Serializable;
  */
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class LoginMessageDTO implements Serializable {
 
     private static final long serialVersionUID = 1L;
