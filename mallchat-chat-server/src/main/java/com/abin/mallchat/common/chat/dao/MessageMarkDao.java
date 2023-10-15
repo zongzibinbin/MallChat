b8d3cb6d9181dddf02bd3b4b963f453e6505a8d8
djc <1002637814@qq.com>
2023-10-15 11:51:43 +0800

    fix: Room.isRoomFriend

--- mallchat-chat-server/src/main/java/com/abin/mallchat/common/chat/dao/MessageMarkDao.java
+++ mallchat-chat-server/src/main/java/com/abin/mallchat/common/chat/dao/MessageMarkDao.java
@@ -2,7 +2,7 @@ package com.abin.mallchat.common.chat.dao;
 
 import com.abin.mallchat.common.chat.domain.entity.MessageMark;
 import com.abin.mallchat.common.chat.mapper.MessageMarkMapper;
-import com.abin.mallchat.common.common.domain.enums.YesOrNoEnum;
+import com.abin.mallchat.common.common.domain.enums.NormalOrNoEnum;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
 
@@ -29,14 +29,14 @@ public class MessageMarkDao extends ServiceImpl<MessageMarkMapper, MessageMark> 
     public Integer getMarkCount(Long msgId, Integer markType) {
         return lambdaQuery().eq(MessageMark::getMsgId, msgId)
                 .eq(MessageMark::getType, markType)
-                .eq(MessageMark::getStatus, YesOrNoEnum.NO.getStatus())
+                .eq(MessageMark::getStatus, NormalOrNoEnum.NORMAL.getStatus())
                 .count();
     }
 
     public List<MessageMark> getValidMarkByMsgIdBatch(List<Long> msgIds) {
         return lambdaQuery()
                 .in(MessageMark::getMsgId, msgIds)
-                .eq(MessageMark::getStatus, YesOrNoEnum.NO.getStatus())
+                .eq(MessageMark::getStatus, NormalOrNoEnum.NORMAL.getStatus())
                 .list();
     }
 }
